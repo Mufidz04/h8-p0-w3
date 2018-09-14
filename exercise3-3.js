a9883f3c1b5ba1354e19de259e75a43c3b5c52c9
Mufidz04 <Mufidmutashim@gmail.com>
2018-09-14 08:43:49 +0700

    fixedgit add exercise3-3.js

--- exercise3-3.js
+++ exercise3-3.js
@@ -1,72 +1,73 @@
 var input = ["0001", "Roman Alamsyah ", "Bandar Lampung", "21/05/1989", "Membaca"];
 function dataHandling2() {
-    input.splice(1, 2 , "Roman Alamsyah Elsharawy" , "Provinsi Bandar Lampung");
-    input.splice(4, 5 , "Pria", "SMA Internasional Metro");
+    input.splice(1, 2, "Roman Alamsyah Elsharawy", "Provinsi Bandar Lampung");
+    input.splice(4, 5, "Pria", "SMA Internasional Metro");
     console.log(input)
+    //=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+    var bulan = input[3].split('/') // [3]? Karena array bulan terdapat di 3
+    /*
+    Menghasilkan [ '21' , '05' , '1989']
+    */
+    switch (bulan[1]) {
+        // Bulan[1] ? Karena hasil dari split array bulan di 1
+        case '01':
+            bulan = 'Januari'
+            break;
+        case '02':
+            bulan = 'Februari'
+            break;
+        case '03':
+            bulan = 'Maret'
+            break;
+        case '04':
+            bulan = 'April'
+            break;
+        case '05':
+            bulan = 'Mei'
+            break;
+        case '06':
+            bulan = 'Juni'
+            break;
+        case '07':
+            bulan = 'Juli'
+            break;
+        case '08':
+            bulan = 'Agustus'
+            break;
+        case '09':
+            bulan = 'September'
+            break;
+        case '10':
+            bulan = 'Oktober'
+            break;
+        case '11':
+            bulan = 'November'
+            break;
+        case '12':
+            bulan = 'Desember'
+            break;
+        default:
+            bulan = 'Bulan tidak tersedia'
+            break;
+    }
+    console.log(bulan)
+
+    //-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+    var arr = ['21', '05', '1989']
+    arr.sort(function (a, b) { return b - a });
+    console.log(arr)
+
+    //-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+    var formatTanggal = ['21', '05', '1989']
+    console.log(formatTanggal.join('-'))
+
+    //-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+    console.log(input[1].slice(0, 15))
 }
-var bulan = input[3].split('/') // [3]? Karena array bulan terdapat di 3
-/*
-Menghasilkan [ '21' , '05' , '1989']
-*/
-switch(bulan[1]){
-    // Bulan[1] ? Karena hasil dari split array bulan di 1
-    case '01': 
-      bulan = 'Januari'
-      break;
-    case '02':
-      bulan = 'Februari'
-      break;
-    case '03':
-      bulan = 'Maret'
-      break;
-    case '04':
-      bulan = 'April'
-      break;
-    case '05':
-      bulan = 'Mei'
-      break;
-    case '06':
-      bulan = 'Juni'
-      break;
-    case '07':
-      bulan = 'Juli'
-      break;
-    case '08':
-      bulan = 'Agustus'
-      break;
-    case '09':
-      bulan = 'September'
-      break;
-    case '10':
-      bulan = 'Oktober'
-      break;
-    case '11':
-      bulan = 'November'
-      break;
-    case '12':
-      bulan = 'Desember'
-      break;
-    default:
-      bulan = 'Bulan tidak tersedia'
-      break;
-  }
-console.log(bulan)
-
-//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-
-var arr = [ '21' , '05' , '1989']
-arr.sort(function(a, b){return b-a});
-console.log(arr)
-
-//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-
-var formatTanggal = [ '21' , '05' , '1989']
-console.log(formatTanggal.join('-'))
-
-//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-
-console.log(input[1].slice(0, 15))
-dataHandling2(input);
+    dataHandling2(input);
 /**
  * keluaran yang diharapkan (pada console)
  *

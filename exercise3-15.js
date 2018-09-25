8768ccf1bfac312369339db4bd568ecb9cd74304
Mufidz04 <Mufidmutashim@gmail.com>
2018-09-25 09:24:26 +0700

    improvements

--- exercise3-15.js
+++ exercise3-15.js
@@ -1,25 +1,33 @@
 function groupAnimals(animals) {
-  var kelompokA = []
-  var kelompokB = []
-  var kelompokC = []
-  var kelompokD = []
   animals.sort()
-  for ( var i = 0 ; i < animals.length ; i++) {
-    if (animals[i] === 'ayam' || animals[i] === 'anoa') {
-      kelompokA.push(animals[i])
-    } else if ( animals[i] === 'cacing' || animals[i] === 'cicak') {
-      kelompokB.push(animals[i])
-    } else if ( animals[i]=== 'kancil' || animals[i] === 'kuda') {
-      kelompokC.push(animals[i])
+  // Mengurutkan array secara ascending a-z
+  var penampunganBesar = []
+   // variable u/ menampung array yang lebih kecil
+  var alphabetPertama = animals[0][0]
+  // console.log(alphabetPertama)
+  var penampunganKecil = []
+  for (let i = 0; i < animals.length; i++) {
+    if (animals[i][0] === alphabetPertama) {
+      penampunganKecil.push(animals[i])
+         /*jika nilai index 0 dari array i sama dengan nilai dari alphabetPertama maka array tersebut dimasukan 
+        ke penampungan kecil yaitu variable kandang kecil   
+        */ 
     } else {
-      kelompokD.push(animals[i])
+      penampunganBesar.push(penampunganKecil)
+      penampunganKecil = []
+      alphabetPertama = animals[i][0]
+      penampunganKecil.push(animals[i])
     }
+    if (i === animals.length - 1) {
+      penampunganBesar.push(penampunganKecil)
+    }
+    // console.log(penampunganKecil)
   }
-  return [kelompokA , kelompokB , kelompokC , kelompokD]
-  }
+  return penampunganBesar
+}
 
-  // TEST CASES
-console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil']));
+// TEST CASES
+// console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil']));
 // [ ['ayam', 'anoa'], ['cacing'], ['kuda', 'kancil'] ]
-console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil', 'unta', 'cicak' ]));
+console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil', 'unta', 'cicak']));
 // [ ['ayam', 'anoa'], ['cacing', 'cicak'], ['kuda'], ['unta'] ]
